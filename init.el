cde4ab05a833e8e384ad58620a6026e1212b57a6
Jason Reed <jcreed@gmail.com>
2021-01-02 10:09:00 -0500

    Added patrickt-style literate wrapper

---
+++ init.el
@@ -0,0 +1,31 @@
+;;; init.el --- jcreed's emacs setup, derived from Patrick Thomson's.  -*- lexical-binding: t; -*-
+;;
+;;; Commentary:
+;; This file loads use-package, org-mode, and compiles and executes readme.org
+;;
+;;; Code:
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'org-install)
+(require 'ob-tangle)
+
+(defun reload-config ()
+  "Reload the literate config from ~/.config/emacs/readme.org."
+  (interactive)
+  (org-babel-load-file "~/.config/emacs/emacs-config.org"))
+
+(setq max-lisp-eval-depth 2000)
+
+(reload-config)
+
+;;; init.el ends here
