4a9884fb3e59d4ba0affd091c417baf4c5b1dd91
Jason Reed <jcreed@gmail.com>
2021-09-21 15:36:05 -0400

    Fix another dumb bug

--- notes-mode.el
+++ notes-mode.el
@@ -304,8 +304,8 @@
 			 (t (jcreed-browse-thing-at-point-fallback)))))
 
 (defun jcreed-find-file-prefix (file prefix-arg)
-  (cond ((eq prefix-arg 0) (jcreed-find-file-other-window file))
-		  ((eq prefix-arg 0) (find-file file))))
+  (cond ((eq prefix-arg 1) (jcreed-find-file-other-window file))
+		  (t (find-file file))))
 
 (defun jcreed-open-repo-path (repo path prefix-arg)
   (message (concat path " - " repo))
